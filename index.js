d72165148f0a77fe8fac87a4cc369ad42ea5957e
wcn1720811650 <1720811650@qq.com>
2024-12-16 13:15:21 +0000

    Added Movies Route

--- index.js
+++ index.js
@@ -2,8 +2,10 @@ import express from 'express';
 import dotenv from 'dotenv';
 import cors from 'cors';
 import usersRouter from './api/users';
+import moviesRouter from './api/movies';   //import movies router
 import './db';
 import defaultErrHandler from './errHandler'
+
 
 dotenv.config();
 
@@ -13,6 +15,8 @@ const port = process.env.PORT;
 app.use(cors());
 app.use(express.json());
 app.use('/api/users', usersRouter);
+app.use('/api/movies', moviesRouter); //ADD THIS BEFORE THE DEFAULT ERROR HANDLER.
+
 app.use(defaultErrHandler);
 
 app.listen(port, () => {
