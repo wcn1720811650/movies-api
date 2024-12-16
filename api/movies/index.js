7639eb5f8e56caebd3a1a039aee584555e55986a
wcn1720811650 <1720811650@qq.com>
2024-12-16 13:58:27 +0000

    Authentication lab - exercise (genres)

--- api/movies/index.js
+++ api/movies/index.js
@@ -1,7 +1,7 @@
 import movieModel from './movieModel';
 import asyncHandler from 'express-async-handler';
 import express from 'express';
-import { getUpcomingMovies } from '../tmdb-api';
+import { getUpcomingMovies, getGenres } from '../tmdb-api';
 
 const router = express.Router();
 
@@ -39,6 +39,10 @@ router.get('/:id', asyncHandler(async (req, res) => {
 router.get('/tmdb/upcoming', asyncHandler(async (req, res) => {
     const upcomingMovies = await getUpcomingMovies();
     res.status(200).json(upcomingMovies);
+}));
+router.get('/tmdb/genres', asyncHandler(async (req, res) => {
+    const genres = await getGenres();
+    res.status(200).json(genres);
 }));
 
 export default router;
